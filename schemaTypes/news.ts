10d1d2bf27bb4226e424a528da48662dbc444ffe
Marc Ernest Mariano <78241292+marcernest21@users.noreply.github.com>
2025-07-21 13:48:01 +0800

    feat(news): Added slug field to news schema
    Added  'slug' field to the news schema for improved URL generation and referencing.

--- schemaTypes/news.ts
+++ schemaTypes/news.ts
@@ -1,83 +1,87 @@
-import { defineField, defineType } from 'sanity'
+import {defineField, defineType} from 'sanity'
 
 export default defineType({
-    name: 'news',
-    title: 'News',
-    type: 'document',
-    fields: [
-        defineField({
-            name: 'title',
-            title: 'Title',
-            type: 'string',
-        }),
-        defineField({
-            name: 'date',
-            title: 'Date',
-            type: 'date',
-            options: {
-                dateFormat: 'YYYY-MM-DD'
-            }
-        }),
-        defineField({
-            name: 'description',
-            title: 'Description',
-            type: 'array',
-            of: [
-                {
-                    type: 'block'
-                },
-                {
-                    type: 'image',
-                    options: {
-                        hotspot: true,
-                    },
-                    fields: [
-                        {
-                            type: 'string',
-                            name: 'alt',
-                            title: 'Alternative text',
-                            options: {
-                                isHighlighted: true
-                            }
-
-                        },
-                        {
-                            type: 'string',
-                            name: 'caption',
-                            title: 'Caption',
-                            options: {
-                                isHighlighted: true
-                            }
-                        }
-                    ]
-                }
-            ]
-        }),
-        defineField({
-            name: 'mainImage',
-            title: 'Featured Image',
-            type: 'image',
-            options: {
-                hotspot: true,
-                
-            }
-        }),
-    ],
-    orderings: [
+  name: 'news',
+  title: 'News',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'date',
+      title: 'Date',
+      type: 'date',
+      options: {
+        dateFormat: 'YYYY-MM-DD',
+      },
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [
         {
-            title: 'Newest',
-            name: 'releaseDateDesc',
-            by: [
-                { field: 'date', direction: 'desc' }
-            ]
-        }, {
-            title: 'Oldest',
-            name: 'releaseDateAsc',
-            by: [
-                { field: 'date', direction: 'asc' }
-            ]
+          type: 'block',
         },
-    ]
+        {
+          type: 'image',
+          options: {
+            hotspot: true,
+          },
+          fields: [
+            {
+              type: 'string',
+              name: 'alt',
+              title: 'Alternative text',
+              options: {
+                isHighlighted: true,
+              },
+            },
+            {
+              type: 'string',
+              name: 'caption',
+              title: 'Caption',
+              options: {
+                isHighlighted: true,
+              },
+            },
+          ],
+        },
+      ],
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Featured Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 100,
+      },
+    }),
+  ],
+  orderings: [
+    {
+      title: 'Newest',
+      name: 'releaseDateDesc',
+      by: [{field: 'date', direction: 'desc'}],
+    },
+    {
+      title: 'Oldest',
+      name: 'releaseDateAsc',
+      by: [{field: 'date', direction: 'asc'}],
+    },
+  ],
 })
 
 /*    preview: {
@@ -92,5 +96,3 @@ export default defineType({
         },
     }, 
 */
-
-
